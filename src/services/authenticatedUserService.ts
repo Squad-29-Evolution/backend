eb372722e87aa205b0aefec22cb553facbb8e8ec
Sallys Carlos <sallys.carlos93@gmail.com>
2022-11-05 23:11:59 -0300

    feat: create authenticate service

---
+++ src/services/authenticatedUserService.ts
@@ -0,0 +1,35 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { client } from "../prisma/client";
+
+interface IRequestAuthenticated {
+  email: string;
+  password: string;
+}
+
+class AuthenticatedUserService {
+  async execute({ email, password }: IRequestAuthenticated) {
+    const userAlreadyExists = await client.users.findFirst({
+      where: { email },
+    });
+
+    if (!userAlreadyExists) {
+      throw new Error("Email or password incorrect.");
+    }
+
+    const passwordCorrect = await compare(password, userAlreadyExists.password);
+
+    if (!passwordCorrect) {
+      throw new Error("Email or password incorrect.");
+    }
+
+    const token = await sign({}, "2cbe207a-befb-42f9-8cd7-64a8af8f1994", {
+      subject: userAlreadyExists.id,
+      expiresIn: "30d",
+    });
+
+    return { token };
+  }
+}
+
+export { AuthenticatedUserService };
