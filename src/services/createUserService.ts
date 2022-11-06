74db4b9a06f86209aa51147920f1ae1f67feb957
Sallys Carlos <sallys.carlos93@gmail.com>
2022-11-05 22:45:42 -0300

    feat: add client prisma file and createUserService

---
+++ src/services/createUserService.ts
@@ -0,0 +1,35 @@
+import { hash } from "bcrypt";
+import { client } from "../prisma/client";
+
+interface IRequestCreateUser {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  async execute({ name, email, password }: IRequestCreateUser) {
+    const userAlreadyExists = await client.users.findFirst({
+      where: { email },
+    });
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists!");
+    }
+
+    const passwordHash = await hash(password, 8);
+
+    const user = await client.users.create({
+      data: {
+        name,
+        email,
+        password: passwordHash,
+        xp: 0,
+      },
+    });
+
+    return user;
+  }
+}
+
+export { CreateUserService };
