eddcba2933d2d45f47bdc3ddb760b53c7490c679
Sallys Carlos <sallys.carlos93@gmail.com>
2022-11-12 10:44:49 -0300

    feat: add middleware in user route

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { UserController } from "../controllers/userController";
+import { authMiddleware } from "../middleware/authMiddleware";
 
 export const userRouter = Router();
 
@@ -9,5 +10,5 @@ userRouter.post("/users/create", userController.create);
 userRouter.post("/users/login", userController.login);
 userRouter.get("/users/:id", userController.getUserById);
 userRouter.get("/users", userController.getAllUsers);
-userRouter.put("/users/:id", userController.updateUserById);
-userRouter.delete("/users/:id", userController.deleteUserById);
+userRouter.put("/users/:id", authMiddleware, userController.updateUserById);
+userRouter.delete("/users/:id", authMiddleware, userController.deleteUserById);
