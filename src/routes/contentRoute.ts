7188705293f10ef5d2e3558b90ee3fb58e1f71d5
Sallys Carlos <sallys.carlos93@gmail.com>
2022-11-12 10:37:11 -0300

    feat: add middleware in content route

--- src/routes/contentRoute.ts
+++ src/routes/contentRoute.ts
@@ -1,12 +1,25 @@
 import { Router } from "express";
 import { ContentController } from "../controllers/contentController";
+import { adminMiddleware } from "../middleware/adminMiddleware";
 
 export const contentRouter = Router();
 
 const contentController = new ContentController();
 
-contentRouter.post("/contents/create", contentController.create);
+contentRouter.post(
+  "/contents/create",
+  adminMiddleware,
+  contentController.create,
+);
 contentRouter.get("/contents", contentController.getAll);
 contentRouter.get("/contents/:id", contentController.getUnique);
-contentRouter.delete("/contents/:id", contentController.deleteContent);
-contentRouter.put("/contents/:id", contentController.updateContent);
+contentRouter.delete(
+  "/contents/:id",
+  adminMiddleware,
+  contentController.deleteContent,
+);
+contentRouter.put(
+  "/contents/:id",
+  adminMiddleware,
+  contentController.updateContent,
+);
