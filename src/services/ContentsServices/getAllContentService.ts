2603396e43d094dbd202aa7448e3eec98f7a57bf
gustavo parlandim <gustavo.miguel.dias.parlandim@gmail.com>
2022-11-14 21:09:53 -0300

    feat: return content  category

--- src/services/ContentsServices/getAllContentService.ts
+++ src/services/ContentsServices/getAllContentService.ts
@@ -2,7 +2,21 @@ import { client } from "../../prisma/client";
 
 class GetAllContentService {
   async execute() {
-    const content = client.contents.findMany();
+    const content = client.contents.findMany({
+      select: {
+        id: true,
+        link: true,
+        title: true,
+        trail_id: true,
+        type: true,
+        description: true,
+        trail: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
 
     return content;
   }
--- src/services/ContentsServices/updateContentService.ts
+++ src/services/ContentsServices/updateContentService.ts
@@ -32,6 +32,19 @@ class UpdateContentService {
         trail_id,
         category_id,
       },
+      select: {
+        id: true,
+        link: true,
+        title: true,
+        trail_id: true,
+        type: true,
+        description: true,
+        trail: {
+          select: {
+            name: true,
+          },
+        },
+      },
     });
 
     return content;
